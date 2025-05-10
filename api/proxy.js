f4622b8a0b7515ab1d6eed059e5f3a2a8cfb62e9
DivyViradiya07 <divya.viradiya.7@gmail.com>
2025-05-10 17:14:07 +0530

    Initial Proxy-API

---
+++ api/proxy.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+    try {
+        const response = await fetch('http://103.251.17.166:8003/GetStock/AllStock');
+        const data = await response.text();
+
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET');
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).send(data);
+    } catch (err) {
+        res.status(500).json({ error: 'Proxy error', details: err.message });
+    }
+}
