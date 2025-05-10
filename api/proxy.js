715a5fbfba6dcfa6804292eef46cd723dbf0b3df
DivyViradiya07 <divya.viradiya.7@gmail.com>
2025-05-10 17:19:33 +0530

    Third Iteration

--- api/proxy.js
+++ api/proxy.js
@@ -1,8 +1,18 @@
 export default async function handler(req, res) {
-  const response = await fetch('http://103.251.17.166:8003/GetStock/AllStock');
-  const data = await response.text();
+  try {
+    const response = await fetch('http://103.251.17.166:8003/GetStock/AllStock', {
+      method: 'GET',
+    });
 
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).send(data);
+    const data = await response.text();
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Content-Type', 'application/json');
+    
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(500).json({ error: 'Proxy error', details: err.message });
+  }
 }
